5581f3385f83247e73a0e4df1c8fe528f8d2126d
swanhell <swanhell@sfu.ca>
2018-02-14 04:21:02 -0800

    save users to users.json

--- readme.txt
+++ readme.txt
@@ -1,2 +1,5 @@
 References used:
 https://stackoverflow.com/questions/1346716/how-do-i-make-firefox-auto-refresh-on-file-change
+https://stackoverflow.com/questions/2496710/writing-files-in-node-js
+https://stackoverflow.com/questions/13542667/create-directory-when-writing-to-file-in-node-js
+https://stackoverflow.com/questions/14391690/how-to-capture-no-file-for-fs-readfilesync
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+"use strict";
+
 const http = require('http');
 const fs = require('fs');
+const qs = require('querystring');
+
 // const port = 24606;
 const port = 8080;
+const dataDir = __dirname + '/data/';
+const usersFile = dataDir + 'users.json';
+
 const server = http.createServer();
 
 server.on('request', (req, res) => {
@@ -10,7 +17,7 @@ server.on('request', (req, res) => {
             if (req.method === 'GET') {
                 returnFile('form.html', 'text/html', res)
             } else if (req.method === 'POST') {
-
+                handlePost(req, res);
             }
 
             break;
@@ -24,7 +31,63 @@ server.on('request', (req, res) => {
             res.writeHead(404);
             res.end();
     }
-})
+});
+
+function handlePost(req, res) {
+    let body = '';
+
+    req.on('data', (data) => {
+        body += data.toString();
+    });
+
+    req.on('end', () => {
+        let user = parseUser(qs.parse(body));
+        let users = [];
+        
+        if (!fs.existsSync(dataDir)) {
+            fs.mkdirSync(dataDir);
+        } else {
+            try {
+                let content = fs.readFileSync(usersFile);
+                users = JSON.parse(content);
+            } catch (err) {
+                // do nothing
+            }
+        }
+
+        users.push(user);
+
+        fs.writeFile(usersFile, JSON.stringify(users), function (err) {
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log("The user was saved!");
+        });
+
+        returnFile('form.html', 'text/html', res);
+    });
+}
+
+// turns { email: 1, email1: 2, email2: 3 }
+// into { emails: [1, 2, 3] }
+function parseUser(user) {
+    let emails = [];
+    let newUser = {};
+    
+    for (let key in user) {
+        let value = user[key];
+        
+        if (key.startsWith('email')) {
+            emails.push(value);
+        } else {
+            newUser[key] = value;
+        }
+    }
+
+    newUser.emails = emails;
+    return newUser;
+}
 
 function returnFile(fileName, mimeType, res) {
     fs.readFile(fileName, (err, data) => {
